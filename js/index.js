43749fb13b6f33ceac2a6caeb60fdd979c53e062
HazelPan <sea39801991@gmail.com>
2018-04-27 23:34:16 +0800

    add finish function into index.js

--- js/index.js
+++ js/index.js
@@ -17,7 +17,29 @@ class BaseCharacter {
     if (this.hp <= 0) {
       this.die();
     }
+
+    var _this = this;
+    var i = 1;
+
+    _this.id = setInterval(function() {
+      if (i == 1) {
+        _this.element.getElementsByClassName("effect-image")[0].style.display = "block";
+        _this.element.getElementsByClassName("hurt-text")[0].classList.add("attacked");
+        _this.element.getElementsByClassName("hurt-text")[0].textContent = damage;
+      }
+
+      _this.element.getElementsByClassName("effect-image")[0].src = 'images/effect/blade/' + i + '.png';
+      i++;
+      
+      if (i > 8) {
+        _this.element.getElementsByClassName("effect-image")[0].style.display = "none";
+        _this.element.getElementsByClassName("hurt-text")[0].classList.remove("attacked");
+        _this.element.getElementsByClassName("hurt-text")[0].textContent = "";
+        clearInterval(_this.id);
+      }
+    }, 50);
   }
+
   die() {
     this.alive = false;
   }
@@ -79,11 +101,12 @@ var rounds = 10;
 var hero = new Hero("Bernard", 130, 30);
 var monster = new Monster("Skeleton", 130, 10);
 
+
 function endTurn() {
   rounds--;
   document.getElementById("round-num").textContent = rounds;
-  if (rounds < 1) {
-
+  if (rounds < 1) { 
+    finish();
   }
 }
 
@@ -106,11 +129,15 @@ function heroAttack() {
         monster.attack(hero);
         monster.element.classList.remove("attacking");
         endTurn();
-        if (hero.alive == false) {} else {
+        if (hero.alive == false) {
+          finish();
+        } else {
           document.getElementsByClassName("skill-block")[0].style.display = "block";
         }
       }, 500);
-    } else {}
+    } else {
+      finish();
+    }
   }, 1100);
 
 }
@@ -122,3 +149,14 @@ function addSkillEvent() {
   }
 }
 addSkillEvent();
+
+function finish(){
+   var dialog = document.getElementById("dialog");
+   dialog.style.display = "block";
+      
+   if (monster.alive == false){
+     dialog.classList.add("win");
+   }else{
+     dialog.classList.add("lose");
+   }
+}
