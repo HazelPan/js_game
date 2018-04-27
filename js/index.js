df4616be94e34fee7d8e65ad3acf0347bd521741
HazelPan <sea39801991@gmail.com>
2018-04-27 16:32:15 +0800

    update the the progress of fighting in index.js

--- js/index.js
+++ js/index.js
@@ -1,43 +1,36 @@
-   
-class BaseCharacter{
-  constructor(name, hp, ap){
+class BaseCharacter {
+  constructor(name, hp, ap) {
     this.name = name;
     this.hp = hp;
     this.maxHp = hp;
     this.ap = ap;
     this.alive = true;
   }
-
-  attack(character, damage){
-
-    if(this.alive == false){
+  attack(character, damage) {
+    if (this.alive == false) {
       return;
     }
     character.getHurt(damage);
   }
-
-  getHurt(damage){
+  getHurt(damage) {
     this.hp -= damage;
-    if (this.hp <= 0){
-       this.die();
+    if (this.hp <= 0) {
+      this.die();
     }
-   }
-
+  }
   die() {
     this.alive = false;
   }
-
   updateHtml(hpElement, hurtElement) {
     hpElement.textContent = this.hp;
     hurtElement.style.width = (100 - this.hp / this.maxHp * 100) + "%";
   }
-
 }
 
-
-class Hero extends BaseCharacter{
-  constructor(name, hp, ap){
+class Hero extends BaseCharacter {
+  constructor(name, hp, ap) {
     super(name, hp, ap);
+
     this.element = document.getElementById("hero-image-block");
     this.hpElement = document.getElementById("hero-hp");
     this.maxHpElement = document.getElementById("hero-max-hp");
@@ -46,27 +39,20 @@ class Hero extends BaseCharacter{
     this.hpElement.textContent = this.hp;
     this.maxHpElement.textContent = this.maxHp;
 
-
-    console.log(this.name + "英雄誕生啦！The Hero is created!")
+    console.log("召喚英雄 " + this.name + "！");
   }
-
-  attack(character){
+  attack(character) {
     var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
     super.attack(character, Math.floor(damage));
   }
-
   getHurt(damage) {
     super.getHurt(damage);
     this.updateHtml(this.hpElement, this.hurtElement);
   }
-
-  
-
 }
 
-
-class Monster extends BaseCharacter{
-  constructor(name, hp, ap){
+class Monster extends BaseCharacter {
+  constructor(name, hp, ap) {
     super(name, hp, ap);
 
     this.element = document.getElementById("monster-image-block");
@@ -77,79 +63,62 @@ class Monster extends BaseCharacter{
     this.hpElement.textContent = this.hp;
     this.maxHpElement.textContent = this.maxHp;
 
-    console.log(this.name + "怪獸來啦！The Monster is coming!")
+    console.log("遇到怪獸 " + this.name + "了！");
   }
-
-  attack(character, damage){
+  attack(character) {
     var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
     super.attack(character, Math.floor(damage));
   }
-
   getHurt(damage) {
     super.getHurt(damage);
     this.updateHtml(this.hpElement, this.hurtElement);
   }
-  
 }
 
 var rounds = 10;
-var hero = new Hero("Alana",130 ,30)
-var monster = new Monster("LALA",130 ,10)
+var hero = new Hero("Bernard", 130, 30);
+var monster = new Monster("Skeleton", 130, 10);
 
+function endTurn() {
+  rounds--;
+  document.getElementById("round-num").textContent = rounds;
+  if (rounds < 1) {
 
-function endTurn(){
-  rounds --;
-  document.getElementById("round-num").textContent = rounds ;
-  if (rounds < 1){
-    /* write down the funtion of the ending the game*/
   }
- }
-
-
-function heroAttack() {
-    document.getElementsByClassName("skill-block")[0].style.display = "none";
-
-    setTimeout(function() {
-      hero.element.classList.add("attacking");
-      setTimeout(function() {
-        hero.attack(monster);
-        hero.element.classList.remove("attacking");
-      }, 500);
-    }, 100);
-
-    setTimeout(function() {
-      if (monster.alive) {
-        monster.element.classList.add("attacking");
-        setTimeout(function() {
-          monster.attack(hero);
-          monster.element.classList.remove("attacking");
-          endTurn();
-          if (hero.alive == false) {} else {
-            document.getElementsByClassName("skill-block")[0].style.display = "block";
-          }
-        }, 500);
-        } else {
-        // 「遊戲結束」空白區
-        }
-      }, 1100);
-    
 }
 
- 
-function addSkillEvent(){
-  var skill = getElementById("skill");
-  skill.onclick = function(){
+function heroAttack() {
+  // Hero 選技能時觸發回合開始
+  document.getElementsByClassName("skill-block")[0].style.display = "none";
+
+  setTimeout(function() {
+    hero.element.classList.add("attacking");
+    setTimeout(function() {
+      hero.attack(monster);
+      hero.element.classList.remove("attacking");
+    }, 500);
+  }, 100);
+
+  setTimeout(function() {
+    if (monster.alive) {
+      monster.element.classList.add("attacking")
+      setTimeout(function() {
+        monster.attack(hero);
+        monster.element.classList.remove("attacking");
+        endTurn();
+        if (hero.alive == false) {} else {
+          document.getElementsByClassName("skill-block")[0].style.display = "block";
+        }
+      }, 500);
+    } else {}
+  }, 1100);
+
+}
+
+function addSkillEvent() {
+  var skill = document.getElementById("skill");
+  skill.onclick = function() {
     heroAttack();
   }
 }
-
-
 addSkillEvent();
-
-
- 
-
-                
-
-
-
